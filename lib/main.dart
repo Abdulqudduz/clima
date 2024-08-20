e2408bda9e54bcb354bbb8a0463ed0dae86125cb
Abdulqudduz <quddusmuhammed.a@gmail.com>
2024-08-20 04:19:33 -0800

    fourth change

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('man');
+    print('man no gooo');
     return MaterialApp(
       theme: ThemeData.dark(),
       home: LoadingScreen(),
